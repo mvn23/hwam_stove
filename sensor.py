5c2ade9de8738431b6cd333e1347f122fcc59db8
mvn23 <schopdiedwaas@gmail.com>
2025-01-18 12:56:04 +0100

    Use EntityDescription in sensor.py

--- sensor.py
+++ sensor.py
@@ -5,6 +5,7 @@ For more details about this platform, please refer to the documentation at
 https://github.com/mvn23/hwam_stove
 """
 
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 import logging
 
@@ -12,6 +13,7 @@ from homeassistant.components.sensor import (
     ENTITY_ID_FORMAT,
     SensorDeviceClass,
     SensorEntity,
+    SensorEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import PERCENTAGE, UnitOfTemperature
@@ -23,44 +25,112 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 import pystove
 
 from . import CONF_NAME, DATA_HWAM_STOVE, DATA_STOVES
+from .entity import HWAMStoveEntityDescription
 
-SENSOR_DESCRIPTIONS = {
-    # {name: [device_class, unit, friendly_name format]}
-    pystove.DATA_ALGORITHM: [None, None, "Algorithm {}"],
-    pystove.DATA_BURN_LEVEL: [None, None, "Burn Level {}"],
-    pystove.DATA_MAINTENANCE_ALARMS: [None, None, "Maintenance Alarms {}"],
-    pystove.DATA_MESSAGE_ID: [None, None, "Message ID {}"],
-    pystove.DATA_NEW_FIREWOOD_ESTIMATE: [None, None, "New Firewood Estimate {}"],
-    pystove.DATA_NIGHT_BEGIN_TIME: [None, None, "Night Begin Time {}"],
-    pystove.DATA_NIGHT_END_TIME: [None, None, "Night End Time {}"],
-    pystove.DATA_NIGHT_LOWERING: [None, None, "Night Lowering {}"],
-    pystove.DATA_OPERATION_MODE: [None, None, "Operation Mode {}"],
-    pystove.DATA_OXYGEN_LEVEL: [None, PERCENTAGE, "Oxygen Level {}"],
-    pystove.DATA_PHASE: [None, None, "Phase {}"],
-    pystove.DATA_REMOTE_VERSION: [None, None, "Remote Version {}"],
-    pystove.DATA_ROOM_TEMPERATURE: [
-        SensorDeviceClass.TEMPERATURE,
-        UnitOfTemperature.CELSIUS,
-        "Room Temperature {}",
-    ],
-    pystove.DATA_SAFETY_ALARMS: [None, None, "Safety Alarms {}"],
-    pystove.DATA_STOVE_TEMPERATURE: [
-        SensorDeviceClass.TEMPERATURE,
-        UnitOfTemperature.CELSIUS,
-        "Stove Temperature {}",
-    ],
-    pystove.DATA_TIME_SINCE_REMOTE_MSG: [
-        None,
-        None,
-        "Time Since Remote Message {}",
-    ],
-    pystove.DATA_DATE_TIME: [None, None, "Date and time {}"],
-    pystove.DATA_TIME_TO_NEW_FIREWOOD: [None, None, "Time To New Firewood {}"],
-    pystove.DATA_VALVE1_POSITION: [None, None, "Valve 1 Position {}"],
-    pystove.DATA_VALVE2_POSITION: [None, None, "Valve 2 Position {}"],
-    pystove.DATA_VALVE3_POSITION: [None, None, "Valve 3 Position {}"],
-    pystove.DATA_FIRMWARE_VERSION: [None, None, "Firmware Version {}"],
-}
+
+@dataclass(frozen=True, kw_only=True)
+class HWAMStoveSensorEntityDescription(
+    SensorEntityDescription,
+    HWAMStoveEntityDescription,
+):
+    """Describes a hwam_stove sensor entity."""
+
+
+SENSOR_DESCRIPTIONS = [
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_ALGORITHM,
+        name_format="Algorithm {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_BURN_LEVEL,
+        name_format="Burn Level {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_MAINTENANCE_ALARMS,
+        name_format="Maintenance Alarms {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_MESSAGE_ID,
+        name_format="Message ID {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_NEW_FIREWOOD_ESTIMATE,
+        name_format="New Firewood Estimate {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_NIGHT_BEGIN_TIME,
+        name_format="Night Begin Time {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_NIGHT_END_TIME,
+        name_format="Night End Time {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_NIGHT_LOWERING,
+        name_format="Night Lowering {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_OPERATION_MODE,
+        name_format="Operation Mode {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_OXYGEN_LEVEL,
+        native_unit_of_measurement=PERCENTAGE,
+        name_format="Oxygen Level {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_PHASE,
+        name_format="Phase {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_REMOTE_VERSION,
+        name_format="Remote Version {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_ROOM_TEMPERATURE,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        name_format="Room Temperature {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_SAFETY_ALARMS,
+        name_format="Safety Alarms {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_STOVE_TEMPERATURE,
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+        name_format="Stove Temperature {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_TIME_SINCE_REMOTE_MSG,
+        name_format="Time Since Remote Message {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_DATE_TIME,
+        name_format="Date and time {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_TIME_TO_NEW_FIREWOOD,
+        name_format="Time To New Firewood {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_VALVE1_POSITION,
+        name_format="Valve 1 Position {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_VALVE2_POSITION,
+        name_format="Valve 2 Position {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_VALVE3_POSITION,
+        name_format="Valve 3 Position {}",
+    ),
+    HWAMStoveSensorEntityDescription(
+        key=pystove.DATA_FIRMWARE_VERSION,
+        name_format="Firmware Version {}",
+    ),
+]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -74,34 +144,30 @@ async def async_setup_entry(
 
     stove_name = config_entry.data[CONF_NAME]
     stove_device = hass.data[DATA_HWAM_STOVE][DATA_STOVES][stove_name]
-    sensors = []
-    for var, sensor in SENSOR_DESCRIPTIONS.items():
-        device_class = sensor[0]
-        unit = sensor[1]
-        name_format = sensor[2]
-        entity_id = async_generate_entity_id(
-            ENTITY_ID_FORMAT, f"{var}_{stove_device.name}", hass=hass
+    async_add_entities(
+        HwamStoveSensor(
+            stove_device,
+            description,
+            async_generate_entity_id(
+                ENTITY_ID_FORMAT, f"{description.key}_{stove_device.name}", hass=hass
+            ),
         )
-        sensors.append(
-            HwamStoveSensor(
-                entity_id, stove_device, var, device_class, unit, name_format
-            )
-        )
-    async_add_entities(sensors)
+        for description in SENSOR_DESCRIPTIONS
+    )
 
 
 class HwamStoveSensor(SensorEntity):
     """Representation of a HWAM Stove sensor."""
 
-    def __init__(self, entity_id, stove_device, var, device_class, unit, name_format):
+    def __init__(self, stove_device, entity_description, entity_id):
         """Initialize the sensor."""
         self._stove_device = stove_device
         self.entity_id = entity_id
-        self._var = var
+        self._var = entity_description.key
         self._value = None
-        self._device_class = device_class
-        self._unit = unit
-        self._name_format = name_format
+        self._device_class = entity_description.device_class
+        self._unit = entity_description.native_unit_of_measurement
+        self._name_format = entity_description.name_format
 
     async def async_added_to_hass(self):
         """Subscribe to updates from the component."""

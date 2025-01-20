0936046a2da927d8695d701d7d3cd26516ab57af
mvn23 <schopdiedwaas@gmail.com>
2025-01-20 19:55:44 +0100

    Add base entity (#23)
    * Move stove initialization to __init__.py
    
    * Add HWAMStoveBaseEntity class
    
    * Add stove property to HWAMStoveBaseEntity class
    
    * Use explicit class names when calling parent __init__ methods of HWAMStoveCoordinatorEntity for added clarity

--- custom_components/hwam_stove/entity.py
+++ custom_components/hwam_stove/entity.py
@@ -1,9 +1,12 @@
 """Common HWAM Stove entity properties."""
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ID
 from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.entity import EntityDescription
+from homeassistant.helpers.entity import Entity, EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from pystove import Stove
 
 from .const import DOMAIN, StoveDeviceIdentifier
 from .coordinator import StoveCoordinator
@@ -15,20 +18,21 @@ class HWAMStoveEntityDescription(EntityDescription):
     device_identifier: StoveDeviceIdentifier
 
 
-class HWAMStoveCoordinatorEntity(CoordinatorEntity[StoveCoordinator]):
-    """Represent a hwam_stove coordinator entity."""
+class HWAMStoveBaseEntity(Entity):
+    """Represent a hwam_stove base entity."""
 
     _attr_has_entity_name = True
     entity_description: HWAMStoveEntityDescription
+    stove: Stove
 
     def __init__(
         self,
-        stove_coordinator: StoveCoordinator,
+        stove,
+        config_entry: ConfigEntry,
         entity_description: HWAMStoveEntityDescription,
     ) -> None:
         """Initialize the entity."""
-        super().__init__(stove_coordinator)
-        hub_id = stove_coordinator.config_entry.data[CONF_ID]
+        hub_id = config_entry.data[CONF_ID]
         self._attr_unique_id = f"{hub_id}-{entity_description.key}"
         self._attr_device_info = DeviceInfo(
             identifiers={
@@ -38,5 +42,25 @@ class HWAMStoveCoordinatorEntity(CoordinatorEntity[StoveCoordinator]):
                 )
             }
         )
-        self.coordinator = stove_coordinator
         self.entity_description = entity_description
+        self.stove = stove
+
+
+class HWAMStoveCoordinatorEntity(
+    HWAMStoveBaseEntity, CoordinatorEntity[StoveCoordinator]
+):
+    """Represent a hwam_stove coordinator entity."""
+
+    def __init__(
+        self,
+        stove_coordinator: StoveCoordinator,
+        entity_description: HWAMStoveEntityDescription,
+    ) -> None:
+        """Initialize the entity."""
+        CoordinatorEntity.__init__(self, stove_coordinator)
+        HWAMStoveBaseEntity.__init__(
+            self,
+            stove_coordinator.stove,
+            stove_coordinator.config_entry,
+            entity_description,
+        )
